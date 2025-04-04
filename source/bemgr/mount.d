645f4c44b3b92a2570946f75b8fd27e4f04e7d71
Jonathan M Davis <jmdavis@jmdavisprog.com>
2025-04-04 02:22:49 -0600

    Move doMount and doUmount into bemgr.mount.

---
+++ source/bemgr/mount.d
@@ -0,0 +1,102 @@
+// Written in the D programming language
+
+/++
+    Copyright: Copyright 2025.
+    License:   $(WEB www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
+    Authors:   $(HTTP jmdavisprog.com, Jonathan M Davis)
+  +/
+module bemgr.mount;
+
+int doMount(string[] args)
+{
+    enum helpMsg =
+`bemgr mount <beName> <mountpoint>
+
+  Mounts the given boot environment at the given mountpoint.
+  It has no effect on the mountpoint property of the dataset.
+`;
+    import std.exception : enforce;
+    import std.file : exists, isDir;
+    import std.format : format;
+    import std.getopt : getopt;
+    import std.path : buildPath;
+    import std.process : esfn = escapeShellFileName;
+    import std.stdio : writeln;
+
+    import bemgr.util : getPoolInfo, runCmd;
+
+    bool help;
+
+    getopt(args, "help", &help);
+
+    if(help)
+    {
+        writeln(helpMsg);
+        return 0;
+    }
+
+    enforce(args.length == 4, helpMsg);
+
+    immutable beName = args[2];
+    immutable mountpoint = args[3];
+
+    auto poolInfo = getPoolInfo();
+    immutable dataset = buildPath(poolInfo.beParent, beName);
+
+    runCmd(format!`zfs list %s`(esfn(dataset)), format!"Error: %s does not exist"(dataset));
+    enforce(mountpoint.exists, format!"Error: %s does not exist"(mountpoint));
+    enforce(mountpoint.isDir, format!"Error: %s is not a directory"(mountpoint));
+
+    version(FreeBSD)
+        runCmd(format!"mount -t zfs %s %s"(esfn(dataset), esfn(mountpoint)));
+    else version(linux)
+        runCmd(format!"mount -t zfs -o zfsutil %s %s"(esfn(dataset), esfn(mountpoint)));
+    else
+        static assert(false, "Unsupport OS");
+
+    return 0;
+}
+
+int doUmount(string[] args)
+{
+    enum helpMsg =
+`bemgr umount <beName>
+bemgr unmount <beName>
+
+  Unmounts the given inactive boot environment.
+  It does not support forcefully unmounting, because zfs umount does not support
+  it on Linux, but if you know where the mountpoint is, then
+`;
+    import std.exception : enforce;
+    import std.format : format;
+    import std.getopt : getopt;
+    import std.path : buildPath;
+    import std.process : esfn = escapeShellFileName;
+    import std.stdio : writeln;
+
+    import bemgr.util : getPoolInfo, isMounted, runCmd;
+
+    bool help;
+
+    getopt(args, "help", &help);
+
+    if(help)
+    {
+        writeln(helpMsg);
+        return 0;
+    }
+
+    enforce(args.length == 3, helpMsg);
+
+    immutable beName = args[2];
+
+    auto poolInfo = getPoolInfo();
+    immutable dataset = buildPath(poolInfo.beParent, beName);
+
+    runCmd(format!`zfs list %s`(esfn(dataset)), format!"Error: %s does not exist"(dataset));
+    enforce(isMounted(dataset), format!"Error: %s is not mounted"(dataset));
+
+    runCmd(format!"zfs umount %s"(esfn(dataset)));
+
+    return 0;
+}
--- source/realapp.d
+++ source/realapp.d
@@ -36,6 +36,7 @@ Use --help on individual commands for more information.`;
     import bemgr.create : doCreate;
     import bemgr.destroy : doDestroy;
     import bemgr.list : doList;
+    import bemgr.mount : doMount, doUmount;
 
     try
     {
@@ -270,55 +271,6 @@ int doImport(string[] args)
     return 0;
 }
 
-int doMount(string[] args)
-{
-    enum helpMsg =
-`bemgr mount <beName> <mountpoint>
-
-  Mounts the given boot environment at the given mountpoint.
-  It has no effect on the mountpoint property of the dataset.
-`;
-    import std.exception : enforce;
-    import std.file : exists, isDir;
-    import std.format : format;
-    import std.getopt : getopt;
-    import std.path : buildPath;
-    import std.process : esfn = escapeShellFileName;
-    import std.stdio : writeln;
-
-    import bemgr.util : getPoolInfo, runCmd;
-
-    bool help;
-
-    getopt(args, "help", &help);
-
-    if(help)
-    {
-        writeln(helpMsg);
-        return 0;
-    }
-
-    enforce(args.length == 4, helpMsg);
-
-    immutable beName = args[2];
-    immutable mountpoint = args[3];
-
-    auto poolInfo = getPoolInfo();
-    immutable dataset = buildPath(poolInfo.beParent, beName);
-
-    runCmd(format!`zfs list %s`(esfn(dataset)), format!"Error: %s does not exist"(dataset));
-    enforce(mountpoint.exists, format!"Error: %s does not exist"(mountpoint));
-    enforce(mountpoint.isDir, format!"Error: %s is not a directory"(mountpoint));
-
-    version(FreeBSD)
-        runCmd(format!"mount -t zfs %s %s"(esfn(dataset), esfn(mountpoint)));
-    else version(linux)
-        runCmd(format!"mount -t zfs -o zfsutil %s %s"(esfn(dataset), esfn(mountpoint)));
-    else
-        static assert(false, "Unsupport OS");
-
-    return 0;
-}
 
 int doRename(string[] args)
 {
@@ -356,50 +308,6 @@ int doRename(string[] args)
     immutable target = buildPath(poolInfo.beParent, newBE);
 
     runCmd(format!"zfs rename %s %s"(esfn(source), esfn(target)));
-
-    return 0;
-}
-
-int doUmount(string[] args)
-{
-    enum helpMsg =
-`bemgr umount <beName>
-bemgr unmount <beName>
-
-  Unmounts the given inactive boot environment.
-  It does not support forcefully unmounting, because zfs umount does not support
-  it on Linux, but if you know where the mountpoint is, then
-`;
-    import std.exception : enforce;
-    import std.format : format;
-    import std.getopt : getopt;
-    import std.path : buildPath;
-    import std.process : esfn = escapeShellFileName;
-    import std.stdio : writeln;
-
-    import bemgr.util : getPoolInfo, isMounted, runCmd;
-
-    bool help;
-
-    getopt(args, "help", &help);
-
-    if(help)
-    {
-        writeln(helpMsg);
-        return 0;
-    }
-
-    enforce(args.length == 3, helpMsg);
-
-    immutable beName = args[2];
-
-    auto poolInfo = getPoolInfo();
-    immutable dataset = buildPath(poolInfo.beParent, beName);
-
-    runCmd(format!`zfs list %s`(esfn(dataset)), format!"Error: %s does not exist"(dataset));
-    enforce(isMounted(dataset), format!"Error: %s is not mounted"(dataset));
-
-    runCmd(format!"zfs umount %s"(esfn(dataset)));
 
     return 0;
 }
