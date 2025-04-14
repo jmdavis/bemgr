f12279b7301bbc1b4ecfc920672d73c029d0e819
Jonathan M Davis <jmdavis@jmdavisprog.com>
2025-04-14 01:34:33 -0600

    Add the man page to what's installed with install.sh.

--- install.sh
+++ install.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
-echo Clean build...
+echo dub clean
 dub clean
 
-echo Build release build...
 if which ldc2 > /dev/null
 then
+    echo dub build --build=release --compiler=ldc2
     dub build --build=release --compiler=ldc2 || exit 1
 else
+    echo dub build --build=release --compiler=dmd
     dub build --build=release --compiler=dmd || exit 1
 fi
 
 echo sudo cp bemgr /usr/local/sbin/
 sudo cp bemgr /usr/local/sbin/
+
+echo sudo mkdir -p /usr/local/share/man/man8
+sudo mkdir -p /usr/local/share/man/man8
+
+echo gzip -k bemgr.8
+gzip -k bemgr.8
+
+echo sudo mv bemgr.8.gz /usr/local/share/man/man8/
+sudo mv bemgr.8.gz /usr/local/share/man/man8/
